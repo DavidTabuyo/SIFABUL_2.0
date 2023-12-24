a118d68ff7d519e9eee30dd275f44c65b89b22b2
DavidTabuyo <dtabum00@estudiantes.unileon.es>
2023-12-24 13:24:23 +0100

    añadidas interfaces usuario en .py

--- src/controller/login_controller.py
+++ src/controller/login_controller.py
@@ -1,10 +1,12 @@
 from PyQt5.QtWidgets import QMainWindow
-
+from view.login_ui import Ui_MainWindow
 
 class LoginController(QMainWindow):
     def __init__(self, main_controller) -> None:
         super().__init__()
         self.main_controller = main_controller
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
         
     def aceptar(self):
         if valid():
