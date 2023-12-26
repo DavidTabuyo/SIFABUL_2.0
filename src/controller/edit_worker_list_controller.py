1996835458ecf4155d0daf7372d7d23bb9dc297e
DavidTabuyo <dtabum00@estudiantes.unileon.es>
2023-12-26 21:15:39 +0100

    ultimo botón ->edit worker list

--- src/controller/edit_worker_list_controller.py
+++ src/controller/edit_worker_list_controller.py
@@ -1,10 +1,18 @@
 from PyQt5.QtWidgets import QMainWindow
 
+from model.dao.admin_dao import AdminDao
+
 
 class EditWorkerListController(QMainWindow):
-    def __init__(self, main_controller) -> None:
+    def __init__(self, main_controller, admin_id:str) -> None:
         super().__init__()
         self.main_controller = main_controller
+        
+    
+        #view
+        
+        #model
+        self.admin= AdminDao.get_admin(admin_id)
 
 
 
